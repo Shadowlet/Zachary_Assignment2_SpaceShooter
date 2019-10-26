2b635b5bf1f5aff9a3515064875dec22fed5c435
Zachary Carreiro <43147444+Shadowlet@users.noreply.github.com>
2019-10-26 19:39:32 -0400

    Player ship is animated.

--- Assets/PlayerShip.cs
+++ Assets/PlayerShip.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerShip : MonoBehaviour
 {
     Rigidbody2D my_Rigidbody;
-
+    Animator my_animator;
     public float moveSpeed;
+
+    private bool isNeutral = true;
 
 
     // Start is called before the first frame update
     void Start()
     {
         my_Rigidbody = GetComponent<Rigidbody2D>();
+        my_animator = GetComponent<Animator>();
+
+        my_animator.SetBool("IsNeutral", true);
     }
 
     // Update is called once per frame
@@ -22,16 +28,56 @@ public class PlayerShip : MonoBehaviour
         {
             MoveShip();
         }
+        else
+        {
+           
+        }
+
+        
+
+        Debug.Log("Horizontal: " + Input.GetAxisRaw("Horizontal"));
+       Debug.Log("Vertical: " + Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        AnimateShip();
     }
 
     private void MoveShip()
     {
         Vector3 shipMovement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         my_Rigidbody.transform.position += shipMovement * moveSpeed * Time.deltaTime;
+
+        
+    }
+
+    private void AnimateShip()
+    {
+        // Turning Left
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            my_animator.SetBool("IsNeutral", false);
+            my_animator.SetBool("TurningLeft", true);
+        }
+        // Turning right
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+
+            my_animator.SetBool("IsNeutral", false);
+            my_animator.SetBool("TurningRight", true);
+        }
+        else if (Input.GetAxisRaw("Horizontal") == 0)
+        {
+            my_animator.SetBool("IsNeutral", true);
+            my_animator.SetBool("TurningLeft", false);
+            my_animator.SetBool("TurningRight", false);
+        }
     }
 
     private void ShootBullets()
     {
 
     }
+
 }
