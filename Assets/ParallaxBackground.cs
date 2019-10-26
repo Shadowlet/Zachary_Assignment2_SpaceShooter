b2342ac93523e9e5fa6c46e4c33860969fb88e12
Zachary Carreiro <43147444+Shadowlet@users.noreply.github.com>
2019-10-26 17:22:11 -0400

    Player movement done, background added.

---
+++ Assets/ParallaxBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    private Vector2 startPosition;
+    private float updatePosition;
+
+    public float scrollSpeed;
+    public float tileSize;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updatePositon = Mathf.Repeat(Time.time * tileSize);
+    }
+}
---
+++ Assets/PlayerShip.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip : MonoBehaviour
+{
+    Rigidbody2D my_Rigidbody;
+
+    public float moveSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        my_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            MoveShip();
+        }
+    }
+
+    private void MoveShip()
+    {
+        Vector3 shipMovement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        my_Rigidbody.transform.position += shipMovement * moveSpeed * Time.deltaTime;
+    }
+
+    private void ShootBullets()
+    {
+
+    }
+}
